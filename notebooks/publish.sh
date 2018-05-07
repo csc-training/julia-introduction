a0380941245234783a9a53daa127acf291a1023c
natj <nattila.joonas@gmail.com>
2018-05-07 17:33:19 +0200

    control flow

--- notebooks/publish.sh
+++ notebooks/publish.sh
@@ -3,10 +3,12 @@
 jupyter nbconvert ../notebooks/00_notebooks.ipynb --to slides --reveal-prefix ../reveal.js
 jupyter nbconvert ../notebooks/01_intro-to-julia.ipynb --to slides --reveal-prefix ../reveal.js
 jupyter nbconvert ../notebooks/02_basics.ipynb --to slides --reveal-prefix ../reveal.js
+jupyter nbconvert ../notebooks/03_control-flow.ipynb --to slides --reveal-prefix ../reveal.js
 
 
 mv 00_notebooks.slides.html ../slides
 mv 01_intro-to-julia.slides.html ../slides
 mv 02_basics.slides.html ../slides
+mv 03_control-flow.slides.html ../slides
 
 

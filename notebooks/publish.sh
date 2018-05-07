e61da1731a99170151b04108e566f6b77a16a22f
natj <nattila.joonas@gmail.com>
2018-05-07 18:47:01 +0200

    functions

--- notebooks/publish.sh
+++ notebooks/publish.sh
@@ -4,11 +4,13 @@ jupyter nbconvert ../notebooks/00_notebooks.ipynb --to slides --reveal-prefix ..
 jupyter nbconvert ../notebooks/01_intro-to-julia.ipynb --to slides --reveal-prefix ../reveal.js
 jupyter nbconvert ../notebooks/02_basics.ipynb --to slides --reveal-prefix ../reveal.js
 jupyter nbconvert ../notebooks/03_control-flow.ipynb --to slides --reveal-prefix ../reveal.js
+jupyter nbconvert ../notebooks/04_functions.ipynb --to slides --reveal-prefix ../reveal.js
 
 
 mv 00_notebooks.slides.html ../slides
 mv 01_intro-to-julia.slides.html ../slides
 mv 02_basics.slides.html ../slides
 mv 03_control-flow.slides.html ../slides
+mv 04_functions.slides.html ../slides
 
 

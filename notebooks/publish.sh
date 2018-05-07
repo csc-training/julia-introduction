3c5f05dafdcace53c51968a45653ed2c3b05d4be
natj <nattila.joonas@gmail.com>
2018-05-07 15:47:50 +0200

    basics

--- notebooks/publish.sh
+++ notebooks/publish.sh
@@ -2,9 +2,11 @@
 
 jupyter nbconvert ../notebooks/00_notebooks.ipynb --to slides --reveal-prefix ../reveal.js
 jupyter nbconvert ../notebooks/01_intro-to-julia.ipynb --to slides --reveal-prefix ../reveal.js
+jupyter nbconvert ../notebooks/02_basics.ipynb --to slides --reveal-prefix ../reveal.js
 
 
 mv 00_notebooks.slides.html ../slides
 mv 01_intro-to-julia.slides.html ../slides
+mv 02_basics.slides.html ../slides
 
 

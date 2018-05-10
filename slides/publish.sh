303cf699e66afa7cf99990a57aa8fc62f15a098f
natj <nattila.joonas@gmail.com>
2018-05-10 13:17:50 +0200

    hpc

--- slides/publish.sh
+++ slides/publish.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-jupyter nbconvert ../notebooks/00_notebooks.ipynb --to slides --reveal-prefix ../reveal.js
-jupyter nbconvert ../notebooks/01a_intro-to-julia.ipynb --to slides --reveal-prefix ../reveal.js
-jupyter nbconvert ../notebooks/01b_basics.ipynb --to slides --reveal-prefix ../reveal.js
-jupyter nbconvert ../notebooks/02_control-flow.ipynb --to slides --reveal-prefix ../reveal.js
-jupyter nbconvert ../notebooks/03_functions.ipynb --to slides --reveal-prefix ../reveal.js
-jupyter nbconvert ../notebooks/04_io.ipynb --to slides --reveal-prefix ../reveal.js
+jupyter nbconvert ../notebooks/00_notebooks.ipynb             --to slides --reveal-prefix ../reveal.js
+jupyter nbconvert ../notebooks/01a_intro-to-julia.ipynb       --to slides --reveal-prefix ../reveal.js
+jupyter nbconvert ../notebooks/01b_basics.ipynb               --to slides --reveal-prefix ../reveal.js
+jupyter nbconvert ../notebooks/02_control-flow.ipynb          --to slides --reveal-prefix ../reveal.js
+jupyter nbconvert ../notebooks/03_functions.ipynb             --to slides --reveal-prefix ../reveal.js
+jupyter nbconvert ../notebooks/04_io.ipynb                    --to slides --reveal-prefix ../reveal.js
 jupyter nbconvert ../notebooks/05_development-practises.ipynb --to slides --reveal-prefix ../reveal.js
-jupyter nbconvert ../notebooks/06_ecosystem.ipynb --to slides --reveal-prefix ../reveal.js
-jupyter nbconvert ../notebooks/07_hpc.ipynb --to slides --reveal-prefix ../reveal.js
+jupyter nbconvert ../notebooks/06_ecosystem.ipynb             --to slides --reveal-prefix ../reveal.js
+jupyter nbconvert ../notebooks/07_ecosystem-II.ipynb          --to slides --reveal-prefix ../reveal.js
+jupyter nbconvert ../notebooks/08_hpc.ipynb                   --to slides --reveal-prefix ../reveal.js
 
 
 mv ../notebooks/00_notebooks.slides.html .
@@ -19,6 +20,7 @@ mv ../notebooks/03_functions.slides.html .
 mv ../notebooks/04_io.slides.html .
 mv ../notebooks/05_development-practises.slides.html .
 mv ../notebooks/06_ecosystem.slides.html .
-mv ../notebooks/07_hpc.slides.html .
+mv ../notebooks/07_ecosystem-II.slides.html .
+mv ../notebooks/08_hpc.slides.html .
 
 
